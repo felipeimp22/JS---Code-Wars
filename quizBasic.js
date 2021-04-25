2d0ca39933f1e5c41132d118d5e83aae5a290189
felipeimp22 <felipeimperio.imperioo@gmail.com>
2021-04-25 17:54:42 -0300

    QuizBasic

---
+++ quizBasic.js
@@ -0,0 +1,115 @@
+/**
+ *   what is the value result ?
+ */
+const array = [1, 2, 3]
+const result = array[3]
+/**
+ * a) 3
+ * X b) undefined <- right
+ * c) throw a sintaxError
+ * d) null
+ */
+//------------------------------------------------------------------------------
+/**
+ *   what is the value result ?
+ */
+const obj = Object.assign({
+    name: "javascript"
+}, {
+    name: "ECMAScript"
+
+}, {
+    name: "LiveScript"
+}
+)
+const result = obj.name
+/**
+ * a) javaScript
+ * b) LiveScript
+ * X c) ECMAScript <- right
+ * d) undefined
+ * e) Javascript,ECMAScript,LiveScripr
+ */
+
+
+//------------------------------------------------------------------------------
+/**
+ *   what is the value result ?
+ */
+const array = [1, 2, 3, 5]
+const reult = array.indexOf(3)
+/**
+ * X a) 2 <-- right
+ * b) 5
+ * c) -1
+ * d) 3
+ */
+
+
+//------------------------------------------------------------------------------
+/**
+ *   which of this is the same as result += value ?
+ * a) result = value + value
+ * b) result = ++ value
+ * X c) result = result + value <-- right
+ * d) result = value
+ * e) result = value ++
+ */
+
+
+//------------------------------------------------------------------------------
+/**
+ *   what is the value result ?
+ */
+const o = {
+    name: "Brendan"
+}
+const s = JSON.parse(JSON.stringify(o))
+result = s === o
+
+/**
+ * a) true
+ * X b) false <-- right
+ */
+
+//------------------------------------------------------------------------------
+/**
+ *   what is the value result ?
+ */
+const result = "2009" - "1995"
+/**
+ * a) null
+ * b) undefined
+ * c) NaN
+ * X d) 14 <--right
+ * e) Throw a TypeError
+ */
+
+//------------------------------------------------------------------------------
+/**
+ *   which of these IS NOT a logical operator ?
+ * a) &&
+ * X b) | <-right
+ * c) ||
+ * d) !
+ */
+
+//------------------------------------------------------------------------------
+/**
+ *   what is the value result ?
+ */
+const re = /Java/i
+const str = "JavaScript";
+const result = re.test(str)
+
+/**
+ * X a) true <-- right
+ * b) false
+ */
+
+//------------------------------------------------------------------------------
+/**
+ *   what is the value result ?
+ */
+
+//------------------------------------------------------------------------------
